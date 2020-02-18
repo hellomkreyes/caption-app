f7290f2f03c67be1e7ff4845505d8f19888c3679
hellomkreyes <mkgreyes@gmail.com>
2020-02-18 15:46:31 -0500

    reset button

--- src/common/components/ImageUploader/ImageUploader.js
+++ src/common/components/ImageUploader/ImageUploader.js
@@ -15,18 +15,24 @@ class ImageUploader extends Component {
     uploadLabel: 'Upload an image!',
     imagePreviewURL: '',
     rawFile: '',
-    hasImage: false
+    hasImage: false,
+    fileSize: '',
+    rawFileSize: 0,
+    sizeLimit: 4000000,
+    errorMsg: 'Exceeds file size. Choose a smaller image 🙏~'
   }
 
   postImageToApi = () => {
     Apikey.apiKey = REACT_APP_API_KEY
     const api = new cloudmersiveImageApiClient.RecognizeApi()
     const { rawFile } = this.state
-    const { getResponse } = this.props
+    const {
+      getResponse
+    } = this.props
 
     const callback = function (error, data, res) {
       if (error) {
-        console.error(error, res)
+        console.error(error, res.text)
       } else {
         console.log('API called successfully.')
         getResponse(res.body)
@@ -50,18 +56,47 @@ class ImageUploader extends Component {
       this.setState({
         rawFile,
         imagePreviewURL: reader.result,
-        hasImage: true
+        hasImage: true,
+        fileSize: `${this.convertBytes(rawFile.size)}`,
+        rawFileSize: rawFile.size
       })
     }
 
     reader.readAsDataURL(rawFile)
   }
 
+  handleReset = () => {
+    const form = document.getElementById('imageForm')
+    form.reset()
+
+    this.setState({
+      imagePreviewURL: '',
+      rawFile: '',
+      hasImage: false,
+      fileSize: '',
+      rawFileSize: 0,
+    })
+  }
+
+  convertBytes = (bytes) => {
+    // taken from https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return '0 Byte'
+
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
+  }
+
   render() {
     let {
       imagePreviewURL,
       uploadLabel,
-      hasImage
+      hasImage,
+      fileSize,
+      rawFileSize,
+      sizeLimit,
+      errorMsg
     } = this.state
     let $imagePreview
 
@@ -76,19 +111,26 @@ class ImageUploader extends Component {
 
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
-          {!hasImage && <label htmlFor='imageFile' className={'button'}>{uploadLabel}</label>}
-          <input type='file'
-            id='imageFile'
-            name='imageFile'
-            accept='image/png, image/jpeg'
-            onChange={this.handleImageChange}
-            className={'visually-hidden'}
-          />
+        <div className={styles.imageUploader__container}>
+          <form id='imageForm' onSubmit={this.handleSubmit}>
+            {!hasImage && <label htmlFor='imageFile' className={'button'}>{uploadLabel}</label>}
+            <input type='file'
+              id='imageFile'
+              name='imageFile'
+              accept='image/png, image/jpeg'
+              onChange={this.handleImageChange}
+              className={'visually-hidden'}
+            />
 
+            {hasImage && rawFileSize <= sizeLimit && <Button type={'submit'}>Generate Caption!</Button>}
 
-          {hasImage && <Button type={'submit'}>Generate Caption!</Button>}
-        </form>
+            {rawFileSize > sizeLimit && <span className={styles.imageUploader__fileSize}>{fileSize}</span>}
+          </form>
+
+          {hasImage && <Button className={styles.imageUploader__reset} onClick={this.handleReset}>Reset!</Button>}
+        </div>
+
+        {rawFileSize > sizeLimit && <span className={styles.imageUploader__errorMsg}>{errorMsg}</span>}
 
         { $imagePreview }
       </>
