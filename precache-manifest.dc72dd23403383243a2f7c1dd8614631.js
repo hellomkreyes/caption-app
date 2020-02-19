35427be59322e0d28dabdc8d1d88991c26752228
hellomkreyes <mkgreyes@gmail.com>
2020-02-19 16:37:47 -0500

    Updates

---
+++ precache-manifest.dc72dd23403383243a2f7c1dd8614631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f04b19a8e53390a642ad23b0e7ee7e",
+    "url": "/caption-app/index.html"
+  },
+  {
+    "revision": "7cd5b54ed2f5bdef09f7",
+    "url": "/caption-app/static/css/main.26025d79.chunk.css"
+  },
+  {
+    "revision": "2416a87d250bdadcef96",
+    "url": "/caption-app/static/js/2.ab86e501.chunk.js"
+  },
+  {
+    "revision": "7cd5b54ed2f5bdef09f7",
+    "url": "/caption-app/static/js/main.0e94b5ff.chunk.js"
+  },
+  {
+    "revision": "ff596dbef4a5270d74aa",
+    "url": "/caption-app/static/js/runtime-main.9900fd02.js"
+  }
+]);
